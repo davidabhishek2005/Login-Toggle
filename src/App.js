3db6c0ad3c4a71d74ebbb98a29249e1900862704
davidabhishek2005 <davidabhisekhg@gmail.com>
2024-06-10 07:14:51 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
+import { createContext, useState } from 'react';
+import ReactSwitch from "react-switch";
+
+
+
+export const ThemeContext = createContext(null);
 
 function App() {
+
+
+  const [theme,setTheme] = useState("dark");
+
+  const toggleTheme = ()=>{
+    setTheme((curr)=> (curr === "light" ? "dark" : "light"));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{theme,setTheme}}>
+       <div id={theme}>
+         <div className="switch">
+         <label>{theme === "light" ? "Light mode " : "Dark Mode "}</label>
+         <ReactSwitch onChange={toggleTheme} checked ={theme === "dark"}/></div>
+         <LoginSignUp/>
+       </div>
+    </ThemeContext.Provider>
+
   );
 }
 
---
+++ src/components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import './LoginSignUp.css'
+import user_icon from '../assets/person.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
+
+const LoginSignUp = () => {
+   
+    const [action,setAction] = useState("Sign Up");
+
+  return (
+    <div className='container'>
+       <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+       </div>
+       <div className="inputs">
+
+         {action === "Login"?<div></div>:<div className="input">
+            <img src= {user_icon} alt =""></img>
+            <input type="text" placeholder="Name"></input>
+        </div>}
+    
+        <div className="input">
+            <img src={email_icon} alt =""></img>
+            <input type="email" placeholder="Email Id"></input>
+        </div>
+        <div className="input">
+            <img src={password_icon} alt =""></img>
+            <input type="password" placeholder="Password"></input>
+        </div>
+
+        {action === "Sign Up"?<div></div> : 
+            <div className="forgot-password">Lost Password?    <span>Click Here</span></div>}
+        
+
+       </div>  
+       <div className='submit-container'>
+        <div className={action === "Login"? "submit gray":"submit"} onClick = {()=> {setAction("Sign Up")}}>Sign Up</div>
+        <div className={action === "Sign Up"? "submit gray":"submit"} onClick = {()=> {setAction("Login")}}>Login</div>
+       </div>         
+    </div>
+  )
+}
+
+export default LoginSignUp
